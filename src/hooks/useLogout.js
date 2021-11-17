a24af955cbf4a261166b700f46cf4b898cc74754
Daniel O'Brien <daniel@monomania.ie>
2021-11-17 13:51:00 +0000

    Added cleanup function to hooks

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
 export const useLogout = () => {
+  const [isCancelled, setisCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
@@ -17,14 +18,24 @@ export const useLogout = () => {
 
       // dispatch logout action
       dispatch({ type: 'LOGOUT' });
-      setError(null);
-      setIsPending(false);
+
+      // update state
+      if (!isCancelled) {
+        setError(null);
+        setIsPending(false);
+      }
     } catch (err) {
       console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    return () => setisCancelled(true);
+  }, []);
 
   return { logout, error, isPending };
 };
