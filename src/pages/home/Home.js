0e49240dd41a1556e0ad975e80fb85ce5e8a4730
Daniel O'Brien <daniel@monomania.ie>
2021-11-17 19:59:59 +0000

    Added useCollection hook,grab firestore colleciton

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,18 @@
 //hooks
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 //styles
 import styles from './Home.module.css';
 //components
 import TransactionForm from './TransactionForm';
 
 export default function Home() {
+  const { documents, error } = useCollection('transactions');
+
   const { user } = useAuthContext();
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>content</div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
