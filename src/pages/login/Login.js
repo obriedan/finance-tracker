f285f19cd28bbc6f8587de51843d975e1968675a
Daniel O'Brien <daniel@monomania.ie>
2021-11-17 14:02:24 +0000

    added useLogin hook to Login Page.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
 
 //styles
 import styles from './Login.module.css';
@@ -6,10 +7,11 @@ import styles from './Login.module.css';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -23,7 +25,14 @@ export default function Login() {
         <span>password:</span>
         <input type='password' onChange={(e) => setPassword(e.target.value)} value={password} />
       </label>
-      <button className='btn'>Login</button>
+      {isPending ? (
+        <button className='btn' disabled>
+          Loading...
+        </button>
+      ) : (
+        <button className='btn'>Login</button>
+      )}
+      {error && <p className='error'>{error}</p>}
     </form>
   );
 }
