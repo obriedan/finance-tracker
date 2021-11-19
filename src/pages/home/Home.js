e445ca31d18da2174d822c0d3f0c6df52924b4b4
Daniel O'Brien <daniel@monomania.ie>
2021-11-19 14:07:21 +0000

    Transaction List Component Created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,7 @@ import { useCollection } from '../../hooks/useCollection';
 import styles from './Home.module.css';
 //components
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 export default function Home() {
   const { documents, error } = useCollection('transactions');
@@ -12,7 +13,10 @@ export default function Home() {
   const { user } = useAuthContext();
   return (
     <div className={styles.container}>
-      <div className={styles.content}>content</div>
+      <div className={styles.content}>
+        {error && <p className='error'>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
